ba4524e351110409f89aab90382961f40efdd1a3
Your Name <severus.key308@gmail.com>
2023-03-15 22:17:07 +0400

    15 Mar 22'17

--- loops.py
+++ loops.py
@@ -21,6 +21,13 @@ for man in mens:
 age = 25
 num = 0
 
+# while num < age:
+#     print(num)
+#     num += 1
+
 while num < age:
-    print(num)
+    if num % 2 == 0:
+        print(num)
+    if num > 10:
+        break
     num += 1
