7cbe2ffd65e054c18b9760aea984b5a318f99738
Your Name <severus.key308@gmail.com>
2023-03-15 20:51:12 +0400

    15 Mar 20'51

--- if_elif.py
+++ if_elif.py
@@ -1,5 +1,10 @@
 age = int(input("Enter your age :"))
 
 if age < 10:
-    # Code Block
     print('You are young, strange one')
+
+elif age == 10:
+    print('You have the right age to play in our level')
+
+else:
+    print('You\'re too old, strange one')
