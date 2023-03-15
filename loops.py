c0e196cec28f0baae768cdcfde3e5bd43fcd810f
Your Name <severus.key308@gmail.com>
2023-03-15 21:55:17 +0400

    15 Mar 21'55

--- loops.py
+++ loops.py
@@ -1,8 +1,16 @@
 mens = ['ruy', 'gael', 'bron', 'patrick']
 
-for man in mens:
-    print(man)
+# for man in mens:
+#     print(man)
 
 # Slice Method
-for man in mens[0:3]:
-    print(man)
+# for man in mens[0:3]:
+#     print(man)
+
+# Using f-string methods
+for man in mens:
+    if man == 'bron':
+        print(f'{man} - black belt')
+        break
+    else:
+        print(man)
