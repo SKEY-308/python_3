f5bf82331c8192e34bedac49aadfa23bb79be0f4
Your Name <severus.key308@gmail.com>
2023-03-15 19:37:43 +0400

    15 Mar 19'37

--- area_calc.py
+++ area_calc.py
@@ -3,3 +3,9 @@
 # name = input('Tell me more about your name, man :')
 # age = input('...and your age :')
 # print(name + ' you are ' + age + ' years old !!')
+
+# Calc the area of a circle
+radius = input('Enter the radius of the circle (m) :')
+pi = 3.142
+area = pi * int(radius) ** 2
+print('The area of your circle is :', area)
