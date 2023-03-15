a94d874bbca55cfb0ae4b8370717b257f3a8cedc
Your Name <severus.key308@gmail.com>
2023-03-15 19:21:14 +0400

    15 Mar 19'21

--- area_calc.py
+++ area_calc.py
@@ -1,4 +1,5 @@
 # This is a comment in this area
-name = input('Tell me more about your name, man :')
-age = input('...and your age :')
-print(name + ' you are ' + age + ' years old !!')
+
+# name = input('Tell me more about your name, man :')
+# age = input('...and your age :')
+# print(name + ' you are ' + age + ' years old !!')
