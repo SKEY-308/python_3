194309c7ac32471cca303f42402e6e587bb540ca
Your Name <severus.key308@gmail.com>
2023-03-15 20:20:02 +0400

    15 Mar 20'19

--- string_format.py
+++ string_format.py
@@ -2,4 +2,12 @@ num1 = 3.1423433443
 num2 = 10.324422
 
 # Previous
-print("num 1 is", num1, "and num2 is", num2)
+# print("num 1 is", num1, "and num2 is", num2)
+
+#Format Method
+# print('num1 is {0:.3} and num2 is {1:.3}'.format(num1, num2))
+# print('num1 is {0:.3f} and num2 is {1:.3f}'.format(num1, num2))
+
+#Using F-Strings
+print(f"num1 is {num1:.4} and num2 is {num2:.4}")
+print(f"num1 is {num1:.4f} and num2 is {num2:.4f}")
