49200a34eba1949c32b19c3dc158dc5884cbe5a7
Your Name <severus.key308@gmail.com>
2023-03-16 23:08:20 +0400

    16 Mar 23'08

--- classes.py
+++ classes.py
@@ -1,4 +1,4 @@
-# -------------- Classes
+# -------------- Previously
 name = 'Severus'
 age = 30
 num = [1, 2, 3, 4, 5, 6]
@@ -7,3 +7,23 @@ typ_name = type(name)
 typ_age = type(age)
 typ_num = type(num)
 print(typ_age)
+
+
+# -------------- Classes
+class Planet:
+    def __init__(self):
+        self.name = 'Hoth'
+        self.radius = 200000
+        self.gravity = 5.5
+        self.system = 'Hoth System'
+
+    def orbit(self):
+        return f'{self.name} is orbiting in the {self.system}'
+
+
+hoth = Planet()
+print(f'Name is: {hoth.name}')
+print(f'Radius is: {hoth.radius}')
+print(f'Gravity is: {hoth.gravity}')
+print(f'System is: {hoth.system}')
+print(hoth.orbit())
