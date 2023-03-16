35dfaefee0346986f443853f6ea8c99e6037f9c5
Your Name <severus.key308@gmail.com>
2023-03-16 17:02:18 +0400

    16 Mar 17'02

--- dictionnaries.py
+++ dictionnaries.py
@@ -1,6 +1,12 @@
-# dictionnaries
-
 james_belts = {"crystal": "red", "ruy": "black"}
 
 print(james_belts["crystal"])
 print(james_belts["ruy"])
+
+# dictionnaries
+print('yoshi' in james_belts)
+print('ruy' in james_belts)
+
+print(james_belts.keys())
+
+print(list(james_belts.keys()))
