346a8172beccccef595e06c08c0acf9a6a5521a4
Your Name <severus.key308@gmail.com>
2023-03-25 01:30:21 +0400

    25 Mar 01'30

--- textTwo.py
+++ textTwo.py
@@ -1,8 +1,8 @@
 from bar_tab import Tab
 
-table1 = Tab()
+table = Tab()
 
-table1.add_item('soft_drink')
-table1.add_item('chicken')
-table1.add_item('desert')
-table1.print_bill(10, 10)
+table.add_item('soft_drink')
+table.add_item('chicken')
+table.add_item('desert')
+table.print_bill(10, 10)
